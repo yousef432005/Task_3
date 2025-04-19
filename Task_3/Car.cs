4e9777690682b77ad28e1fa966a13cff5dd86121
win 10-11 <win 10-11@DESKTOP-QPJAVDM>
2025-04-20 00:23:17 +0200

    Add project files.

---
+++ Task_3/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class Car
+    {
+        public string brand;
+        public string model;
+        public string year;
+        public string color;
+
+        public Car(string Brand, string Model, string Year,string Color)
+        {
+            this.brand = Brand;
+            this.model = Model;
+            this.year = Year;
+            this.color = Color;
+        }
+        public Car(string Brand, string Model, string Year) 
+        {
+            this.brand = Brand;
+            this.model = Model;
+            this.year = Year;
+            color = "white ";
+        }
+        public Car(string Brand)
+        {
+            this.brand = Brand;
+            this.model = "unknown";
+            this.year = "unknown";
+            color = "black ";
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {brand}, Model: {model}, Year: {year}, Color: {color}");
+        }
+        
+    }
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Task_3;
+
+namespace CIS
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Fiat","128","1965","Red");
+            car1.DisplayInfo();
+
+            Car car2 = new Car("Toyota", "Corolla", "2025");
+            car2.DisplayInfo();
+
+            Car car3 = new Car("Mercedes");
+            car3.DisplayInfo();
+
+            
+
+
+
+        }
+    }
+}
